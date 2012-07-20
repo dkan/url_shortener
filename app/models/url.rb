880d1d2328d74fa6d58e10cfd4a53239e9809806
Damien <damien.kan@gmail.com>
2012-07-20 12:31:23 -0700

    vanity urls

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,19 +1,12 @@
 class Url < ActiveRecord::Base
     attr_accessible :long_url, :short_url, :count
     validates_presence_of :long_url
+    validates :short_url, presence: true, uniqueness: true
     before_save :add_http
     
     def add_http
         self.long_url = ("http://" + self.long_url) unless self.long_url =~ /^http/i
-        self.short_url ||= randomize
         self.count ||= 0
-    end
-    
-    private
-    
-    def randomize
-        chars = (('a'..'z').to_a + ('0'..'9').to_a) - %w(i o 0 1 l 0)
-        (1..8).collect{|a| chars[rand(chars.size)] }.join
     end
     
 end
